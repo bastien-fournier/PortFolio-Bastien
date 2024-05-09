f849942eb26dcfc6e6aa47d13d1aea99be65535d
Bastien Fournier <bastien.fournier06@gmail.com>
2024-05-09 15:52:08 +0200

    update homepage

--- component/HomePage/Button.jsx
+++ component/HomePage/Button.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "../HomePage/button.css";
+
+import Cv from "../../assets/cv-bastien-fournier.pdf";
 
 import { Link } from "react-router-dom";
 
 function Button() {
+  // const [load, setLoad] = useState(null)
+
+  // const upLoad = () => {
+
+  // }
+
   return (
     <div className="div-button">
-      <button type="button" className="button">
-        Télécharger mon CV
-      </button>
+      <a href={Cv} download="Cv">
+        <button type="button" className="button">
+          Télécharger mon CV
+        </button>
+      </a>
       <button type="button" className="button">
         <Link to="/project"> Mes projets</Link>
       </button>
